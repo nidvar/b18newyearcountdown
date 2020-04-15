580d6d48fc19590d3a0a8df3585513ad7d18634b
Jay Kim <djayshine87@hotmail.com>
2020-04-15 18:29:51 +1000

    added next year to the title

--- script.js
+++ script.js
@@ -1,28 +1,9 @@
-const current_date = new Date()
 const destination_date = new Date('Jan 01 2'+1)
-
-const time_difference = destination_date - current_date
-const days = time_difference/1000/60/60/24
-const hours = (days - Math.floor(days)) * 24
-const minutes = (hours-Math.floor(hours))*60
-const seconds = (minutes - Math.floor(minutes))*60
-
-
-const full_day = Math.floor(days)
-const full_hour = Math.floor(hours)
-const full_minutes = Math.floor(minutes)
-const full_seconds = Math.floor(seconds)
-console.log(full_day, full_hour, full_minutes,full_seconds)
-
-document.querySelector('.days').innerHTML = full_day
-document.querySelector('.hours').innerHTML = full_hour
-document.querySelector('.minutes').innerHTML = full_minutes
-document.querySelector('.seconds').innerHTML = full_seconds
-
+const destination_year = destination_date.getFullYear();
+document.getElementById('next_year').innerHTML = destination_year
 const run=()=>{
     setInterval(()=>{
         const current_date = new Date()
-        const destination_date = new Date('Jan 01 2'+1)
 
         const time_difference = destination_date - current_date
         const days = time_difference/1000/60/60/24
